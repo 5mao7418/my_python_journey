632fe36793b9800ed45a39184dcb1a33c7151c36
Lamin Mao <maolemin@hotmail.com>
2025-02-24 17:18:42 -0800

    Added Day 4 script with loops and conditionals

---
+++ day4.py
@@ -0,0 +1,12 @@
+# Day 4: Exploring Loops and Conditionals
+
+# Using a loop to print numbers 1 to 5
+for i in range(1, 6):
+    print("Number:", i)
+
+# Using an if-else statement
+age = 20
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
