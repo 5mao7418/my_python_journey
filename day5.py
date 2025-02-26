bd65649cd2d78a6237b75b601f50403d47803eb5
Lamin Mao <maolemin@hotmail.com>
2025-02-25 20:11:42 -0800

    Updated day5.py to take user input for introduction

--- day5.py
+++ day5.py
@@ -1,9 +1,9 @@
 def introduce_yourself():
-    name = "Leo"
-    hobby = "coding"
-    language = "Python"
+    name = input("What is your name? ")
+    hobby = input("What is your favorate hobby? ")
+    language = input("What is your favorate porgraming language?")
 
-    print(f"Hello! My name is {name}. Ilove {hobby} and my favroit programing language is{language}")
+    print(f"Hello! My name is {name}. Ilove {hobby} and my favroit programing language is {language}")
 
 # Call the function
 introduce_yourself()
