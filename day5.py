a0e192bbfe827f587560509c5aafcd5cf0bcf68f
Lamin Mao <maolemin@hotmail.com>
2025-02-25 20:05:02 -0800

    Fixed typo in day5.py and successfully ran script

---
+++ day5.py
@@ -0,0 +1,9 @@
+def introduce_yourself():
+    name = "Leo"
+    hobby = "coding"
+    language = "Python"
+
+    print(f"Hello! My name is {name}. Ilove {hobby} and my favroit programing language is{language}")
+
+# Call the function
+introduce_yourself()
